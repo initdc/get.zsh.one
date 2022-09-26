81c364aff32ea412b467a29b5edab9d24c2c9352
initdc <initd@outlook.com>
2022-09-26 09:31:13 +0000

    refactor: using`zplug` to load plugins

--- install.sh
+++ install.sh
@@ -1,306 +1,125 @@
 #!/bin/sh
-#
-# This script should be run via curl:
-#   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# or wget:
-#   sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#
-# As an alternative, you can first download the install script and run it afterwards:
-#   wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
-#   sh install.sh
-#
-# You can tweak the install behavior by setting variables when running the script. For
-# example, to change the path to the Oh My Zsh repository:
-#   ZSH=~/.zsh sh install.sh
-#
-# Respects the following environment variables:
-#   ZSH     - path to the Oh My Zsh repository folder (default: $HOME/.oh-my-zsh)
-#   REPO    - name of the GitHub repo to install from (default: ohmyzsh/ohmyzsh)
-#   REMOTE  - full remote URL of the git repo to install (default: GitHub via HTTPS)
-#   BRANCH  - branch to check out immediately after install (default: master)
-#
-# Other options:
-#   CHSH       - 'no' means the installer will not change the default shell (default: yes)
-#   RUNZSH     - 'no' means the installer will not run zsh after the install (default: yes)
-#   KEEP_ZSHRC - 'yes' means the installer will not replace an existing .zshrc (default: no)
-#
-# You can also pass some arguments to the install script to set some these options:
-#   --skip-chsh: has the same behavior as setting CHSH to 'no'
-#   --unattended: sets both CHSH and RUNZSH to 'no'
-#   --keep-zshrc: sets KEEP_ZSHRC to 'yes'
-# For example:
-#   sh install.sh --unattended
-#
-set -e
-
-# Default settings
-ZSH=${ZSH:-~/.oh-my-zsh}
-REPO=${REPO:-ohmyzsh/ohmyzsh}
-REMOTE=${REMOTE:-https://github.com/${REPO}.git}
-BRANCH=${BRANCH:-master}
-
-# Other options
-CHSH=${CHSH:-yes}
-RUNZSH=${RUNZSH:-yes}
-KEEP_ZSHRC=${KEEP_ZSHRC:-no}
-
 
 command_exists() {
-	command -v "$@" >/dev/null 2>&1
+  command -v "$@" > /dev/null
 }
 
-error() {
-	echo ${RED}"Error: $@"${RESET} >&2
+dir_exists() {
+  test -d "$@"
 }
 
-setup_color() {
-	# Only use colors if connected to a terminal
-	if [ -t 1 ]; then
-		RED=$(printf '\033[31m')
-		GREEN=$(printf '\033[32m')
-		YELLOW=$(printf '\033[33m')
-		BLUE=$(printf '\033[34m')
-		BOLD=$(printf '\033[1m')
-		RESET=$(printf '\033[m')
+file_exists() {
+  test -f "$@"
+}
+
+zplug_installer() {
+  if dir_exists $HOME/.zplug; then
+    cd $HOME/.zplug && git pull origin master
+    return
+  fi
+  git clone https://github.com/zplug/zplug $HOME/.zplug
+}
+
+omz_installer() {
+  if dir_exists $HOME/.oh-my-zsh; then
+    cd $HOME/.oh-my-zsh && git pull origin master
+    return
+  fi
+  git clone https://github.com/ohmyzsh/ohmyzsh $HOME/.oh-my-zsh
+}
+
+zshrc_writer() {
+  if file_exists $HOME/.zshrc; then
+
+    file_name=.zshrc
+    current_time=$(date "+%Y.%m.%d_%H:%M:%S")
+    new_name=$file_name"_"$current_time
+
+    mv $HOME/.zshrc $HOME/$new_name
+  fi
+  cat << EOF > $HOME/.zshrc
+source ~/.zplug/init.zsh
+zplug "zplug/zplug", hook-build:"zplug --self-manage"
+zplug "lib/*", from:oh-my-zsh
+zplug "themes/robbyrussell", from:oh-my-zsh, as:theme
+zplug "zsh-users/zsh-autosuggestions"
+zplug "zsh-users/zsh-syntax-highlighting"
+zplug "zsh-users/zsh-completions"
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    fi
+fi
+# Then, source plugins and add commands to PATH
+zplug load #--verbose
+# https://zsh-prompt-generator.site
+PROMPT="%F{green}➜%f %B%F{cyan}%~%f%b "
+PROMPT+='%{\$reset_color%}\$(git_prompt_info)'
+RPROMPT="%B%F{green}%?%f%b%B%F{208} <<%f%b%B%F{240}%*%f%b"
+HTTP='127.0.0.1:1080'
+SOCKS='127.0.0.1:1087'
+default=\$HTTP
+unproxy() {
+    unset http_proxy HTTP_PROXY https_proxy HTTPS_PROXY all_proxy ALL_PROXY ftp_proxy FTP_PROXY dns_proxy DNS_PROXY
+    echo "clear proxy done"
+}
+setproxy() {
+	if [ \$# -eq 0 ]
+	then
+		inArg=\$default
 	else
-		RED=""
-		GREEN=""
-		YELLOW=""
-		BLUE=""
-		BOLD=""
-		RESET=""
+		inArg=\$1
 	fi
+	HOST=\$(echo \$inArg |cut -d: -f1)
+	PORT=\$(echo \$inArg |cut -d: -f2)
+	http_proxy=http://\$HOST:\$PORT
+	HTTP_PROXY=\$http_proxy
+	all_proxy=\$http_proxy
+	ALL_PROXY=\$http_proxy
+	ftp_proxy=\$http_proxy
+	FTP_PROXY=\$http_proxy
+	dns_proxy=\$http_proxy
+	DNS_PROXY=\$http_proxy
+	https_proxy=\$http_proxy
+	HTTPS_PROXY=\$https_proxy
+	no_proxy="localhost, 127.0.0.1, localaddress, 10.*.*.*, 192.168.*.*,"
+	echo "current proxy is \${http_proxy}"
+	export no_proxy http_proxy HTTP_PROXY https_proxy HTTPS_PROXY all_proxy ALL_PROXY ftp_proxy FTP_PROXY dns_proxy DNS_PROXY
 }
-
-setup_ohmyzsh() {
-	# Prevent the cloned repository from having insecure permissions. Failing to do
-	# so causes compinit() calls to fail with "command not found: compdef" errors
-	# for users with insecure umasks (e.g., "002", allowing group writability). Note
-	# that this will be ignored under Cygwin by default, as Windows ACLs take
-	# precedence over umasks except for filesystems mounted with option "noacl".
-	umask g-w,o-w
-
-	echo "${BLUE}Cloning Oh My Zsh...${RESET}"
-
-	command_exists git || {
-		error "git is not installed"
-		exit 1
-	}
-
-	if [ "$OSTYPE" = cygwin ] && git --version | grep -q msysgit; then
-		error "Windows/MSYS Git is not supported on Cygwin"
-		error "Make sure the Cygwin git package is installed and is first on the \$PATH"
-		exit 1
-	fi
-
-	git clone -c core.eol=lf -c core.autocrlf=false \
-		-c fsck.zeroPaddedFilemode=ignore \
-		-c fetch.fsck.zeroPaddedFilemode=ignore \
-		-c receive.fsck.zeroPaddedFilemode=ignore \
-		--depth=1 --branch "$BRANCH" "$REMOTE" "$ZSH" || {
-		error "git clone of oh-my-zsh repo failed"
-		exit 1
-	}
-
-	echo
-}
-
-setup_zshrc() {
-	# Keep most recent old .zshrc at .zshrc.pre-oh-my-zsh, and older ones
-	# with datestamp of installation that moved them aside, so we never actually
-	# destroy a user's original zshrc
-	echo "${BLUE}Looking for an existing zsh config...${RESET}"
-
-	# Must use this exact name so uninstall.sh can find it
-	OLD_ZSHRC=~/.zshrc.pre-oh-my-zsh
-	if [ -f ~/.zshrc ] || [ -h ~/.zshrc ]; then
-		# Skip this if the user doesn't want to replace an existing .zshrc
-		if [ $KEEP_ZSHRC = yes ]; then
-			echo "${YELLOW}Found ~/.zshrc.${RESET} ${GREEN}Keeping...${RESET}"
-			return
-		fi
-		if [ -e "$OLD_ZSHRC" ]; then
-			OLD_OLD_ZSHRC="${OLD_ZSHRC}-$(date +%Y-%m-%d_%H-%M-%S)"
-			if [ -e "$OLD_OLD_ZSHRC" ]; then
-				error "$OLD_OLD_ZSHRC exists. Can't back up ${OLD_ZSHRC}"
-				error "re-run the installer again in a couple of seconds"
-				exit 1
-			fi
-			mv "$OLD_ZSHRC" "${OLD_OLD_ZSHRC}"
-
-			echo "${YELLOW}Found old ~/.zshrc.pre-oh-my-zsh." \
-				"${GREEN}Backing up to ${OLD_OLD_ZSHRC}${RESET}"
-		fi
-		echo "${YELLOW}Found ~/.zshrc.${RESET} ${GREEN}Backing up to ${OLD_ZSHRC}${RESET}"
-		mv ~/.zshrc "$OLD_ZSHRC"
-	fi
-
-	echo "${GREEN}Using the Oh My Zsh template file and adding it to ~/.zshrc.${RESET}"
-
-	sed "/^export ZSH=/ c\\
-export ZSH=\"$ZSH\"
-" "$ZSH/templates/zshrc.zsh-template" > ~/.zshrc-omztemp
-	mv -f ~/.zshrc-omztemp ~/.zshrc
-
-	echo
-}
-
-setup_shell() {
-	# Skip setup if the user wants or stdin is closed (not running interactively).
-	if [ $CHSH = no ]; then
-		return
-	fi
-
-	# If this user's login shell is already "zsh", do not attempt to switch.
-	if [ "$(basename "$SHELL")" = "zsh" ]; then
-		return
-	fi
-
-	# If this platform doesn't provide a "chsh" command, bail out.
-	if ! command_exists chsh; then
-		cat <<-EOF
-			I can't change your shell automatically because this system does not have chsh.
-			${BLUE}Please manually change your default shell to zsh${RESET}
-		EOF
-		return
-	fi
-
-	echo "${BLUE}Time to change your default shell to zsh:${RESET}"
-
-	# Prompt for user choice on changing the default login shell
-	printf "${YELLOW}Do you want to change your default shell to zsh? [Y/n]${RESET} "
-	read opt
-	case $opt in
-		y*|Y*|"") echo "Changing the shell..." ;;
-		n*|N*) echo "Shell change skipped."; return ;;
-		*) echo "Invalid choice. Shell change skipped."; return ;;
-	esac
-
-	# Check if we're running on Termux
-	case "$PREFIX" in
-		*com.termux*) termux=true; zsh=zsh ;;
-		*) termux=false ;;
-	esac
-
-	if [ "$termux" != true ]; then
-		# Test for the right location of the "shells" file
-		if [ -f /etc/shells ]; then
-			shells_file=/etc/shells
-		elif [ -f /usr/share/defaults/etc/shells ]; then # Solus OS
-			shells_file=/usr/share/defaults/etc/shells
-		else
-			error "could not find /etc/shells file. Change your default shell manually."
-			return
-		fi
-
-		# Get the path to the right zsh binary
-		# 1. Use the most preceding one based on $PATH, then check that it's in the shells file
-		# 2. If that fails, get a zsh path from the shells file, then check it actually exists
-		if ! zsh=$(which zsh) || ! grep -qx "$zsh" "$shells_file"; then
-			if ! zsh=$(grep '^/.*/zsh$' "$shells_file" | tail -1) || [ ! -f "$zsh" ]; then
-				error "no zsh binary found or not present in '$shells_file'"
-				error "change your default shell manually."
-				return
-			fi
-		fi
-	fi
-
-	# We're going to change the default shell, so back up the current one
-	if [ -n "$SHELL" ]; then
-		echo $SHELL > ~/.shell.pre-oh-my-zsh
-	else
-		grep "^$USER:" /etc/passwd | awk -F: '{print $7}' > ~/.shell.pre-oh-my-zsh
-	fi
-
-	# Actually change the default shell to zsh
-	if ! chsh -s "$zsh"; then
-		error "chsh command unsuccessful. Change your default shell manually."
-	else
-		export SHELL="$zsh"
-		echo "${GREEN}Shell successfully changed to '$zsh'.${RESET}"
-	fi
-
-	echo
-}
-
-one_req() {
-  sudo apt update && sudo apt install -y zsh
-  sudo apt install -y curl git
-}
-
-one_git() {
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-  git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
-  cd ~/
-  mv .zshrc .zshrc.bak
-  curl -o .zshrc https://get.zsh.one/rc
+# set PATH so it includes user's private bin if it exists
+if [ -d "\$HOME/bin" ] ; then
+    PATH="\$HOME/bin:\$PATH"
+fi
+# set PATH so it includes user's private bin if it exists
+if [ -d "\$HOME/.local/bin" ] ; then
+    PATH="\$HOME/.local/bin:\$PATH"
+fi
+# self_bin() {
+# 	for bin in \$HOME/bin/*/bin; do
+# 	export PATH="\$PATH:\$bin"
+# 	done
+# }
+# self_bin
+# export PATH="\$PATH:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+EOF
 }
 
 main() {
-	# Run as unattended if stdin is closed
-	one_req
+  if !(command_exists zsh); then
+    echo "This script needs zsh installed to run."
+    exit 1
+  fi
 
-	if [ ! -t 0 ]; then
-		RUNZSH=no
-		CHSH=no
-	fi
+  if !(command_exists git); then
+    echo "This script needs git installed to run."
+    exit 1
+  fi
 
-	# Parse arguments
-	while [ $# -gt 0 ]; do
-		case $1 in
-			--unattended) RUNZSH=no; CHSH=no ;;
-			--skip-chsh) CHSH=no ;;
-			--keep-zshrc) KEEP_ZSHRC=yes ;;
-		esac
-		shift
-	done
-
-	setup_color
-
-	if ! command_exists zsh; then
-		echo "${YELLOW}Zsh is not installed.${RESET} Please install zsh first."
-		exit 1
-	fi
-
-	if [ -d "$ZSH" ]; then
-		cat <<-EOF
-			${YELLOW}You already have Oh My Zsh installed.${RESET}
-			You'll need to remove '$ZSH' if you want to reinstall.
-		EOF
-		exit 1
-	fi
-
-	setup_ohmyzsh
-	setup_zshrc
-	setup_shell
-	printf "$GREEN"
-	cat <<-'EOF'
-		         __                                     __
-		  ____  / /_     ____ ___  __  __   ____  _____/ /_
-		 / __ \/ __ \   / __ `__ \/ / / /  /_  / / ___/ __ \
-		/ /_/ / / / /  / / / / / / /_/ /    / /_(__  ) / / /
-		\____/_/ /_/  /_/ /_/ /_/\__, /    /___/____/_/ /_/
-		                        /____/                       ....is now installed!
-
-
-		Please look over the ~/.zshrc file to select plugins, themes, and options.
-
-		p.s. Follow us on https://twitter.com/ohmyzsh
-
-		p.p.s. Get stickers, shirts, and coffee mugs at https://shop.planetargon.com/collections/oh-my-zsh
-
-	EOF
-	printf "$RESET"
-
-	one_git
-
-	if [ $RUNZSH = no ]; then
-		echo "${YELLOW}Run zsh to try it out.${RESET}"
-		exit
-	fi
-
-	exec zsh -l
+  omz_installer
+  zplug_installer
+  zshrc_writer
+  zsh
 }
 
 main "$@"
